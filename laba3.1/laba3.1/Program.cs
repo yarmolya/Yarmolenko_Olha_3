212232de2bb74929a25985ced62b0d7a9cb3de1e
yarmolya <107544797+yarmolya@users.noreply.github.com>
2022-06-15 14:41:35 +0200

    Add files via upload

---
+++ laba3.1/laba3.1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace laba3
+{
+    class Program
+    {
+        private static int CompareLinesByLength(string x, string y)
+        {
+            
+                 
+                    int retval = x.Length.CompareTo(y.Length);
+
+                    if (retval != 0)
+                    {
+                        
+                        return retval;
+                    }
+                    else
+                    {
+                      
+                        return x.CompareTo(y);
+                    }
+                
+            
+        }
+
+        private static string SeperateWords (string x)
+        {
+            string[] words = x.Split(' ');
+            string max = "";
+            for (int i = 0; i < words.Length; i++) { 
+                if (words[i].Length > max.Length)
+                {
+                    max = words[i];
+                }
+            }
+            return max;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write the lines of poem and press Enter");
+            var lines = new List<string> { };
+            while (true)
+            {
+                String s = Console.ReadLine();
+                if (s == "")
+                {
+                    break;
+                }
+                else
+                {
+                    lines.Add(s);
+                }
+            }
+
+            Console.WriteLine("Your poem: ");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            lines.Sort(CompareLinesByLength);
+            Console.WriteLine(" ");
+            Console.WriteLine("Your sorted by the length of lines poem: ");
+            foreach (string line in lines)
+            {
+                
+                Console.WriteLine(line);
+                
+            }
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string line = lines[i];
+                lines[i] = SeperateWords(line);
+
+            }
+           
+            string max = "";
+            foreach(string line in lines)
+            {
+                if(line.Length > max.Length)
+                {
+                    max = line;
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("The longest word in your poem is: ");
+            Console.Write(max);
+        }
+
+       
+    }
+}
+
---
+++ laba3.3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+
+{
+ 
+    class MainClass
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting number: ");
+            int start = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ending number: ");
+            int end = Int32.Parse(Console.ReadLine());
+            IEnumerable<int> numbers = Enumerable.Range(start, end);
+            int last = numbers.Last();
+            Console.Write("List of numbers: ");
+            foreach (int i in numbers)
+            {
+                if (i == last)
+                {
+                    Console.Write(i);
+                    Console.Write(".");
+                }
+                else
+                {
+                    Console.Write(i);
+                    Console.Write(", ");
+                }
+            }
+            IEnumerable<int> doubleNum = numbers.Where(c => c > 9 && c < 100);
+
+            if (doubleNum.Any())
+            {
+                int count = 0;
+                foreach (int i in doubleNum)
+                {
+                    count++;
+                }
+                double average = doubleNum.Average();
+                Console.WriteLine(" ");
+                Console.WriteLine("The number of positive double figures: {0}.", count);
+                Console.WriteLine("The average of this numbers: {0}.", average);
+            }
+            else
+            { 
+                Console.WriteLine(" ");
+                Console.WriteLine("The number of positive double figures: 0");
+                Console.WriteLine("The average of this numbers: 0.0");
+            }
+            
+     }
+    }
+}
